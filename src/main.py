c9ac26a72ac51302092e63b80b74a99fb38d9a78
Michael Baluja <mbaluja@ucsd.edu>
2020-07-10 11:02:01 -0700

    add sentiment data, user input option for artist, .gitignore

--- src/main.py
+++ src/main.py
@@ -1,26 +1,53 @@
 import lyricsgenius # Python-based Genius API
-import os # For reading tracklists
-from utils import remove_extension, newlinestrip # Sanitization
+import pandas as pd # For organizing data to analyze
+import utils # For sanitation 
 
-# Authenticate with Genius API
-genius = lyricsgenius.Genius(input('Please enter API key: '))
+# For lyrical analysis
+import numpy as np
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.sentiment.vader import SentimentIntensityAnalyzer 
+from nltk.corpus import stopwords
+#from nltk.stem import PorterStemmer
 
-# Create artist (max_songs = 1 in order to be able to selectively add songs later on)
+analyser = SentimentIntensityAnalyzer()
+stop_words = set(stopwords.words('english'))
+
+# Ignore handful of words that aren't stopwords but should be
+import warnings
+warnings.filterwarnings('ignore')
+
+# Install necessary nltk packages in case not already installed
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('vader_lexicon')
+
+#ps = PorterStemmer()
+# User input
+# Authenticate with Genius API, set conditions
+genius = lyricsgenius.Genius(client_access_token=input('Please enter API key: '),remove_section_headers=True)
+artist_to_search = input('Enter artist to search: ')
+
+# Create artist (max_songs = -1 in order to be able to selectively add songs later on)
 ## Note: Want max_songs = 0 but bug causes all songs to be searched instead of none
-artist = genius.search_artist("Childish Gambino", max_songs=1, sort="title")
+artist = genius.search_artist(artist_to_search, sort="title")
 
-# Read in track lists from data files, create {album : tracklist} dict pairs
-track_dict = dict()
-for file in os.listdir('data'):
-    f = open('data/{}'.format(file), 'r')
-    file = remove_extension(file)
-    track_dict[file] = f.readlines()
+# Create variables to store song info in format easy to work with
+df = pd.DataFrame(columns=['Song', 'Album', 'Year', 'Lyrics'])
 
-# Sanitize tracklists by removing erraneous newline characters
-for key, value in track_dict.items():
-    track_dict[key] = newlinestrip(value)
+# Create dataframe with all necessary song info
+for song in artist.songs[1:]:
+    #year = song.year.split('-')[0]
+    df = df.append({'Song':song.title, 'Album':song.album, 'Year':song.year, 'Lyrics':song.lyrics}, ignore_index=True)
 
-# Add songs from tracklists to artist object
-for album in track_dict.keys():
-    for song in track_dict[album]:
-        artist.add_song(genius.search_song(song, artist.name))
+# Tokenize lyrics & remove stop words
+df.Lyrics = df.Lyrics.apply(word_tokenize)
+df.Lyrics = df.Lyrics.apply(lambda lyrics: [word for word in lyrics if word not in stop_words])
+#df.Lyrics = df.Lyrics.apply(ps.stem)
+
+# Add num_words for each song in order to normalize sentiment
+df['n_words'] = df['Lyrics'].apply(len)
+df['compound'] = df['Lyrics'].apply(lambda lyrics: sum([analyser.polarity_scores(word)['compound'] for word in lyrics])) / df['n_words']
+df['pos'] = df['Lyrics'].apply(lambda lyrics: sum([analyser.polarity_scores(word)['pos'] for word in lyrics])) / df['n_words']
+df['neu'] = df['Lyrics'].apply(lambda lyrics: sum([analyser.polarity_scores(word)['neu'] for word in lyrics])) / df['n_words']
+df['neg'] = df['Lyrics'].apply(lambda lyrics: sum([analyser.polarity_scores(word)['neg'] for word in lyrics])) / df['n_words']
