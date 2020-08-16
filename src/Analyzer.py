ea1761a770c8935c1f699cff9772a2961cf54692
Michael Baluja <mbaluja@ucsd.edu>
2020-08-15 23:02:21 -0700

    add ability to read/write cache files for faster researches

--- src/Analyzer.py
+++ src/Analyzer.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import lib.utils.utils as utils # For sanitation 
+import os
 
 # For graphing
 import lib.utils.grapher as gr
@@ -19,6 +20,8 @@ class Analyzer():
         self.genius = genius
         self.remove_remix = args.remove_remix
         self.remove_unfinished = args.remove_unfinished
+        self.cache = args.cache
+        self.is_cached = args.from_cache
 
     def analyze_artist(self, artist_to_search, stop_words, sentiment_analyzer, by='album'):
         '''
@@ -41,23 +44,30 @@ class Analyzer():
         # Create variables to store song info in format easy to work with
         df = pd.DataFrame(columns=['Song', 'Album', 'Year', 'Lyrics'])
 
-        while True:
-            try:
-                artist = self.genius.search_artist(artist_to_search, sort="title")
-                # Create dataframe with all necessary song info
-                for song in artist.songs:
-                    date = song.year.split('-')[0] if song.year is not None else np.nan
-                    df = df.append({'Song':song.title, 'Album':song.album, 'Date':date, 'Lyrics':song.lyrics}, ignore_index=True)
-                break
-            except AttributeError:
-                artist_to_search = input('Please enter artist to search: ')
+        # Double if statement so that if a cached file is not found, the is_cache variable is set to False, and the data collection takes place
+        if self.is_cached:
+            df = self.from_cache(filename='{}_by_{}'.format(artist_to_search.replace(' ', ''), by))
+        if not self.is_cached:
+            while True:
+                try:
+                    artist = self.genius.search_artist(artist_to_search, sort="title")
+                    # Create dataframe with all necessary song info
+                    for song in artist.songs:
+                        date = song.year.split('-')[0] if song.year is not None else np.nan
+                        df = df.append({'Song':song.title, 'Album':song.album, 'Date':date, 'Lyrics':song.lyrics}, ignore_index=True)
+                    break
+                except AttributeError:
+                    artist_to_search = input('Please enter artist to search: ')
 
-        df = self.tokenize(df, stop_words)
-        df = self.add_sentiment(df, sentiment_analyzer)
+            df = self.tokenize(df, stop_words)
+            df = self.add_sentiment(df, sentiment_analyzer)
 
-        # Assign all songs w/o album to 'noalbum' (at the time of writing, no album listed on genius has the title 'noalbum', so no issue is currently anticipated)
-        df['Album'] = df['Album'].apply(lambda title: 'noalbum' if title is None else title)
+            # Assign all songs w/o album to 'noalbum' (at the time of writing, no album listed on genius has the title 'noalbum', so no issue is currently anticipated)
+            df['Album'] = df['Album'].apply(lambda title: 'noalbum' if title is None else title)
 
+            if self.cache:
+                print('artist:', artist_to_search.strip())
+                self.to_cache(filename='{}_by_{}'.format(artist_to_search.replace(' ', ''), by), df=df)
         # Drop remixed songs 
         df = utils.trim_songs(df, remix=self.remove_remix, unfinished=self.remove_unfinished)
 
@@ -85,26 +95,77 @@ class Analyzer():
         '''
         # Create variables to store song info in format easy to work with
         df = pd.DataFrame(columns=['Song', 'Lyrics'])
+        if self.is_cached:
+            df = self.from_cache(filename=song_to_search.replace(' ', ''))
+        if not self.is_cached:
+            while True:
+                try:
+                    if song_artist is not None:
+                        verbose_state = self.genius.verbose
+                        self.genius.verbose = True
+                    song = [self.genius.search_song(song_to_search, artist=song_artist if song_artist is not None else '')]
+                    # Create dataframe with all necessary song info
+                    df = df.append({'Song':song[0].title, 'Lyrics':song[0].lyrics}, ignore_index=True)
+                    self.genius.verbose = verbose_state
+                    break
+                except AttributeError:
+                    song_to_search = input('Please enter correct song to search: ')
+                    song_artist = input('Please enter song artist (press enter if same artist): ')
 
-        while True:
-            try:
+            df = self.tokenize(df, stop_words)
+            df = self.add_sentiment(df, sentiment_analyzer)
+
+            if self.cache:
                 if song_artist is not None:
-                    verbose_state = self.genius.verbose
-                    self.genius.verbose = True
-                song = [self.genius.search_song(song_to_search, artist=song_artist if song_artist is not None else '')]
-                # Create dataframe with all necessary song info
-                df = df.append({'Song':song[0].title, 'Lyrics':song[0].lyrics}, ignore_index=True)
-                self.genius.verbose = verbose_state
-                break
-            except AttributeError:
-                song_to_search = input('Please enter correct song to search: ')
-                song_artist = input('Please enter song artist (press enter if same artist): ')
+                    filename = '{}_by_{}'.format(song_to_search.replace(' ', ''), song_artist.replace(' ', ''))
+                else:
+                    filename = '{}'.format(song_to_search.replace(' ', ''))
+                self.to_cache(filename=filename, df=df)
 
-        df = self.tokenize(df, stop_words)
-        df = self.add_sentiment(df, sentiment_analyzer)
         self.songs_to_analyze = df.Song.values
 
         return df
+
+    def to_cache(self, filename, df):
+        '''
+        Create a json file from the dataframe
+        
+        Parameters:
+        - artist: (str)
+            - name of artist (for file name)
+        - by: (str)
+            - how data was collected (for file name)
+        - df: (pd.DataFrame)
+            - dataframe to be serialized
+        '''
+        # Create cache directory if not already present
+        if not os.path.isdir('cache'):
+            os.mkdir('cache')
+        
+        df.to_json('cache/{}.json'.format(filename))
+        print('File saved to cache')
+
+    def from_cache(self, filename):
+        '''
+        Create dataframe of data to analyze from previously cached json file
+
+        Parameters:
+        - artist: (str)
+            - title of artist 
+        - by: (str)
+            - how data was collected
+        Return:
+        - df: (pd.DataFrame)
+            - if csv was found, returns content in dataframe
+        '''
+        try:
+            df = pd.read_json('cache/filename.json')
+            return df
+        except FileNotFoundError:
+            print('Cached data could not be found, building from scratch')
+            self.cache = True if input('Would you like to cache this new data? (y/n): ').lower() in ('y', 'yes') else False
+            print('Data will be cached') if self.cache is True else print('Data will not be cached')
+            self.is_cached = False
 
     def tokenize(self, df, stop_words):
         '''
--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ nltk.download('punkt', quiet=True)
 nltk.download('vader_lexicon', quiet=True)
 
 parser = argparse.ArgumentParser(description='Gather lyrical info')
-parser.add_argument('--key', type=str, default=None, help='key for genius api')
+parser.add_argument('--genius_key', type=str, default=None, help='key for genius api')
 parser.add_argument('--artist', type=str, default=None, help='artist to search for (must be surrounded in quotes if multiple words)')
 parser.add_argument('--album', type=str, default=None, help='album to search for (must be surrounded in quotes if multiple words)')
 parser.add_argument('--song', type=str, default=None, help='song to search for (must be surrounded in quotes if multiple words)')
@@ -34,23 +34,25 @@ parser.add_argument('--by', type=str, default=None, help='specifies how to analy
 parser.add_argument('--plot', nargs='+', action='append', help='specifies how to plot the sentiment', choices=['line', 'pie'])
 parser.add_argument('--remove_remix', dest='remove_remix', action='store_true')
 parser.add_argument('--remove_unfinished', dest='remove_unfinished', action='store_true')
+parser.add_argument('--from_cache', dest='from_cache', action='store_true')
+parser.add_argument('--cache', dest='cache', action='store_true')
 
 args = parser.parse_args()
 
 # Request user input if args not used
-if args.key == None:
-    key = input('Please enter API key: ')
+if args.genius_key == None:
+    genius_key = input('Please enter Genius API key: ')
 else: 
-    key = args.key
+    genius_key = args.genius_key
 
 # Access API and make sure key valid
 while True:
     try:
-        genius = lyricsgenius.Genius(client_access_token=key, remove_section_headers=True)
+        genius = lyricsgenius.Genius(client_access_token=genius_key, remove_section_headers=True)
         break
     except TypeError:
         print('Incorrect API key provided')
-        key = input('Please enter API key: ')
+        genius_key = input('Please enter Genius API key: ')
 
 # Create analysis onbject and analyze artist
 analyzer = Analyzer.Analyzer(genius, args)
